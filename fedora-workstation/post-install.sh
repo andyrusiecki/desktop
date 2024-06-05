9bd8c72469d9c116a31df32167a25d6b65bf9dbe
Andy Rusiecki <andy.rusiecki@gmail.com>
2024-06-04 21:43:58 -0400

    re-doing fedora workstation

--- fedora-workstation/post-install.sh
+++ fedora-workstation/post-install.sh
@@ -1,39 +1,17 @@
 #!/bin/bash
 
-while getopts 'hd:p:' opt; do
-  case "$opt" in
-    d)
-      arg="$OPTARG"
-      if [[ "$arg" == "framework" ]]; then
-        device="$arg"
-      fi
-      ;;
-
-    p)
-      arg="$OPTARG"
-      if [[ "$arg" == "sway" ]]; then
-        profile="$arg"
-      fi
-      ;;
-
-    ?|h)
-      echo "Usage: $(basename $0) [-v] [-d device] [-p profile]"
-      echo -e "-d <device>\t runs additional tasks based on the device (available devices: framework)"
-      echo -e "-p <profile>\t runs additional tasks based on the profile (available profiles: sway)\n"
-      exit 1
-      ;;
-  esac
-done
-
 root=$(dirname $(realpath $0))
 
 packages=(
   # base
-  vim
+  adw-gtk3-theme
+  #sushi
+  tailscale
 
   # shell
   fish
   starship
+  neovim
 
   # font dependencies
   curl
@@ -42,22 +20,29 @@ packages=(
   fontconfig
 
   # development
-  awscli
+  awscli2
+  distrobox
   docker-compose
   golang
   jq
   kubectl
   make
   moby-engine
-  python3-pip
 
   # Other applications
+  celluloid
   code
+  dconf-editor
   discord
+  gnome-boxes
   gnome-tweaks
   google-chrome-stable
   libreoffice
+  nextcloud-client
   steam
+  mangohud
+  gamescope
+  timeshift
 )
 
 flatpak_apps=(
@@ -66,9 +51,11 @@ flatpak_apps=(
   com.mattjakeman.ExtensionManager
   com.slack.Slack
   com.spotify.Client
-  com.usebottles.bottles
+  io.github.Foldex.AdwSteamGtk
   io.github.realmazharhussain.GdmSettings
-  net.cozic.joplin_desktop
+  io.missioncenter.MissionCenter
+  md.obsidian.Obsidian
+  me.dusansimic.DynamicWallpaper
   org.gnome.World.PikaBackup
   org.gtk.Gtk3theme.adw-gtk3
   org.signal.Signal
@@ -77,7 +64,9 @@ flatpak_apps=(
 
 systemd_services_root=(
   docker
+  tailscaled
 )
+
 echo "Starting Fedora Workstation Post-Install Tasks..."
 
 # 1. Update dnf conf
@@ -85,67 +74,37 @@ sudo bash -c 'echo "max_parallel_downloads=20" >> /etc/dnf/dnf.conf'
 
 # 2. Enable RPM fusion
 sudo dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf -y update @core
 
 # 3. Add COPR repos
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
-sudo cp $root/assets/starship.repo /etc/yum.repos.d/
-sudo cp $root/assets/kubernetes.repo /etc/yum.repos.d/
-sudo cp $root/assets/vscode.repo /etc/yum.repos.d/
+sudo cp $root/repos/* /etc/yum.repos.d/
 
 # 4. update current packages
 sudo dnf -y --refresh upgrade
-sudo dnf -y group install core
 
 # 5. Install Snapper and dnf plugin
 sudo dnf -y install snapper python3-dnf-plugins-extras-snapper
 sudo snapper --config=root create-config /
+sudo snapper -c root set-config ALLOW_USERS=$USER SYNC_ACL=yes
 
-# 6. Install Multimedia codes
-# currently broken on fedora 38
-#sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-sudo dnf -y groupupdate sound-and-video
+# 6. Install Multimedia codecs
+# - install full ffmpeg
+sudo dnf -y swap ffmpeg-free ffmpeg --allowerasing
+sudo dnf -y update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+sudo dnf -y update @sound-and-video
 
-# 6. Profile
-case $profile in
-  sway)
-    packages+=(
-      brightnessctl
-      dunst
-      gammastep
-      grimshot
-      kitty
-      playerctl
-      sway
-      swaybg
-      swayidle
-      swaylock
-      waybar
-      wofi
-    )
-    ;;
-esac
+# 7. Hardware acceleration
+# - Intel
+# sudo dnf -y install intel-media-driver
 
-# 7. Device
-case $device in
-  framework)
-    packages+=(
-      # graphics
-      ffmpeg-free
-      libavcodec-freeworld
-      intel-media-driver
-    )
+# - AMD
+sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld
+sudo dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
 
-    # fixing brightness keys
-    sudo grubby --update-kernel=ALL --args="module_blacklist=hid_sensor_hub"
-
-    # improve NVMe power saving
-    sudo grubby --update-kernel=ALL --args="nvme.noacpi=1"
-
-    # fixing screen freezes
-    sudo grubby --update-kernel=ALL --args="i915.enable_psr=0"
-    ;;
-esac
+# - NVIDIA
+# sudo dnf install libva-nvidia-driver
 
 # 8. Install packages
 sudo dnf -y install ${packages[@]}
@@ -153,45 +112,42 @@ sudo dnf -y install ${packages[@]}
 # 9. Install flatpak apps
 flatpak install --noninteractive ${flatpak_apps[@]}
 
-mkdir -p ~/.local/share/applications
-# slack
-sed 's/@@u %U @@/--enable-features=UseOzonePlatform,WaylandWindowDecorations,WebRTCPipeWireCapturer --ozone-platform=wayland @@u %U @@/g' /var/lib/flatpak/exports/share/applications/com.slack.Slack.desktop > ~/.local/share/applications/com.slack.Slack.desktop
-
-# postman
-sed 's/@@u %U @@/--enable-features=UseOzonePlatform,WaylandWindowDecorations --ozone-platform=wayland @@u %U @@/g' /var/lib/flatpak/exports/share/applications/com.getpostman.Postman.desktop > ~/.local/share/applications/com.getpostman.Postman.desktop
-
-# 10. Copy config files
-# shell
-mkdir -p ~/.config/fish/
-cp $root/assets/config.fish ~/.config/fish/
-cp $root/assets/starship.toml ~/.config/
-
-# AWS ECR helper config
-mkdir ~/.docker
-cp $root/assets/docker-config.json ~/.docker/config.json
-
-# 11. Add themes
-mkdir -p ~/.local/share/themes/
-
-tmp_dir=$(mktemp -d)
-curl -L https://github.com/lassekongo83/adw-gtk3/releases/download/v4.5/adw-gtk3v4-5.tar.xz --output $tmp_dir/adw-gtk3v4-5.tar.xz
-tar -xf $tmp_dir/adw-gtk3v4-5.tar.xz -C ~/.local/share/themes/
-rm -rf $tmp_dir
+flatpak override --user com.slack.Slack --socket=wayland
+flatpak override --user com.spotify.Client --socket=wayland
+flatpak override --user md.obsidian.Obsidian --socket=wayland
 
 # 12. Add fonts
-mkdir -p ~/.local/share/fonts/
+# - nerd fonts
+nerd_fonts=(
+  FireCode
+  FireMono
+  JetBrainsMono
+  Meslo
+  Noto
+  RobotoMono
+  SourceCodePro
+  Ubuntu
+  UbuntuMono
+)
 
-tmp_dir=$(mktemp -d)
-curl -L https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/JetBrainsMono.zip --output $tmp_dir/JetBrainsMono.zip
-unzip $tmp_dir/JetBrainsMono.zip -d ~/.local/share/fonts/JetBrainsMono/
-rm -rf $tmp_dir
+nerd_tmp=$(mktemp -d)
+for font in ${nerd_fonts[@]}
+do
+  curl -L https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$font.zip --output $nerd_tmp/$font.zip
+  font_dir=$(echo $font | sed 's/\([A-Z]\)/-\L\1/g;s/^-//')
+  unzip $nerd_tmp/$font.zip -d /usr/share/fonts/nerd-$font/
+done
 
-# ms fonts
+rm -rf $nerd_tmp
+
+fc-cache -f /usr/share/fonts
+
+# - ms fonts
 sudo dnf install -y curl cabextract xorg-x11-font-utils fontconfig
 $ sudo rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
 
-# 12. Post install tasks
-chsh -s $(command -v fish)
+# 13. Post install dev tasks
+sudo usermod -s $(command -v fish) $USER
 
 # docker user
 sudo groupadd docker
@@ -200,32 +156,27 @@ sudo usermod -aG docker $USER
 # aws ecr helper
 go install github.com/awslabs/amazon-ecr-credential-helper/ecr-login/cli/docker-credential-ecr-login@latest
 
-# pywal
-sudo pip3 install pywal
-
-# gnome extensions
+# 14. Gnome settings and extensions
 extensions=(
-  AlphabeticalAppGrid@stuarthayhurst
-  appindicatorsupport@rgcjonas.gmail.com
-  bluetooth-quick-connect@bjarosze.gmail.com
-  blur-my-shell@aunetx
-  mediacontrols@cliffniff.github.com
-  notification-banner-reloaded@marcinjakubowski.github.com
-  nightthemeswitcher@romainvigier.fr
-  no-overview@fthx
-  pip-on-top@rafostar.github.com
   user-theme@gnome-shell-extensions.gcampax.github.com
-  Vitals@CoreCoding.com
+  pip-on-top@rafostar.github.com
+  nightthemeswitcher@romainvigier.fr
+  mediacontrols@cliffniff.github.com
+  just-perfection-desktop@just-perfection
+  caffeine@patapon.info
+  appindicatorsupport@rgcjonas.gmail.com
+  system-monitor@gnome-shell-extensions.gcampax.github.com
+  tailscale@joaophi.github.com
 )
-
-shell_version=$(gnome-shell --version | cut -d' ' -f3)
 
 for uuid in ${extensions[@]}
 do
-  info_json=$(curl -sS "https://extensions.gnome.org/extension-info/?uuid=$uuid&shell_version=$shell_version")
-  download_url=$(echo $info_json | jq ".download_url" --raw-output)
+  if gnome-extensions list | grep --quiet $uuid; then
+    break
+  fi
 
-  gnome-extensions install "https://extensions.gnome.org$download_url"
+  busctl --user call org.gnome.Shell.Extensions /org/gnome/Shell/Extensions org.gnome.Shell.Extensions InstallRemoteExtension s $uuid
+
   gnome-extensions enable $uuid
 done
 
@@ -238,25 +189,11 @@ gsettings set org.gnome.desktop.interface font-antialiasing "rgba"
 gsettings set org.gnome.desktop.interface font-hinting "slight"
 gsettings set org.gnome.desktop.interface gtk-theme "adw-gtk3"
 
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-1 "['<SHIFT><SUPER>1']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-2 "['<SHIFT><SUPER>2']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-3 "['<SHIFT><SUPER>3']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-4 "['<SHIFT><SUPER>4']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-5 "['<SHIFT><SUPER>5']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-6 "['<SHIFT><SUPER>6']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-7 "['<SHIFT><SUPER>7']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-8 "['<SHIFT><SUPER>8']"
+# fractional scaling
+# gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"
 
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<SUPER>1']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<SUPER>2']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<SUPER>3']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<SUPER>4']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 "['<SUPER>5']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['<SUPER>6']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-7 "['<SUPER>7']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-8 "['<SUPER>8']"
-
-gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"
+# variable refresh rate
+gsettings set org.gnome.mutter experimental-features "['variable-refresh-rate']"
 
 gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
 
@@ -264,7 +201,20 @@ gsettings set org.gnome.system.location enabled true
 
 gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'org.mozilla.firefox.desktop', 'com.google.Chrome.desktop', 'com.spotify.Client.desktop', 'com.valvesoftware.Steam.desktop', 'com.slack.Slack.desktop', 'net.cozic.joplin_desktop.desktop', 'com.visualstudio.code.desktop', 'org.gnome.Terminal.desktop']"
 
-# TODO: extension settings
+# extension settings
+# just perfection
+gsettings set org.gnome.shell.extensions.just-perfection notification-banner-position 2
+gsettings set org.gnome.shell.extensions.just-perfection startup-status 0
+
+# media controls
+gsettings set org.gnome.shell.extensions.mediacontrols extension-index 1
+gsettings set org.gnome.shell.extensions.mediacontrols extension-position 'Left'
+gsettings set org.gnome.shell.extensions.mediacontrols label-width 300
+gsettings set org.gnome.shell.extensions.mediacontrols show-control-icons-seek-backward false
+gsettings set org.gnome.shell.extensions.mediacontrols show-control-icons-seek-forward false
+
+# pip on top
+gsettings set org.gnome.shell.extensions.pip-on-top stick true
 
 # 13. Enable systemd services
 for i in ${systemd_services_root[@]}
@@ -273,13 +223,6 @@ do
 done
 
 # Done
-echo -e "\nFedora Workstation Post-Install Tasks Complete!\n"
-
-echo -n "Restarting in "
-for i in {5..1}
-do
-  echo -n "$i..."
-  sleep 1
-done
-
-systemctl reboot
+echo ""
+echo  "Fedora Workstation Post-Install Tasks Complete!"
+echo "Please run \"systemctl reboot\" to complete post install tasks."
