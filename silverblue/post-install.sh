2bb6cfae48fdf52d5ee2d8cd7f77569897bf68c0
Andy Rusiecki <andy.rusiecki@gmail.com>
2026-05-03 21:52:31 -0400

    exact runtime

--- silverblue/post-install.sh
+++ silverblue/post-install.sh
@@ -239,7 +239,7 @@ flatpak_apps=(
 flatpak_runtimes=(
   org.gtk.Gtk3theme.adw-gtk3
   org.gtk.Gtk3theme.adw-gtk3-dark
-  org.freedesktop.Platform.VulkanLayer.MangoHud//25.08
+  org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/25.08
 )
 
 echo "Installing new flatpak apps and runtimes from flathub."
